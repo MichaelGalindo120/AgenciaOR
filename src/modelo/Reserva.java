d654b66708da8b01b002e03ff45be168f211383e
Michael Galindo <galindomichael120@gmail.com>
2025-03-21 05:53:46 -0500

    se agrega java.class de cliente y de reserva, ademas formulario interno de clientes y reservas

---
+++ src/modelo/Reserva.java
@@ -0,0 +1,15 @@
+
+package modelo;
+
+import java.sql.*;
+import com.mysql.jdbc.PreparedStatement;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class Reserva {
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+}
